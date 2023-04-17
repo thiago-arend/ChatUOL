e685e4dfd9e7024735692b013fb3906bfcbac7e3
Thiago Frazzon Arend <thiago.arend@gmail.com>
2023-04-17 14:00:40 -0300

    Finalização da funcionalidade de envio de mensagem

--- index.js
+++ index.js
@@ -22,6 +22,8 @@ promise.then((res) => {
 
     //usuario logado com sucesso
     usuarioLogado(usuario);
+
+
 });
 promise.catch((err) => {
     // em caso de erro, recarrega pagina, pedindo dados novamente
@@ -35,6 +37,9 @@ function usuarioLogado(usuario) {
         promise = axios.post("https://mock-api.driven.com.br/api/vm/uol/status", usuario);
         promise.then((res) => {
             console.log(res);
+
+            // atualiza chat com mensagens
+            atualizaChatPeriodico();
         });
         promise.catch((err) => {
             console.log("Erro. Usuário deixou a sala!");
@@ -43,4 +48,72 @@ function usuarioLogado(usuario) {
     }, 5000);
 }
 
+function preparaMensagem() {
+    const msg = document.querySelector(".footer input").value;
+    console.log(msg)
+    enviarMensagem(msg);
+}
 
+function enviarMensagem(msg) {
+    console.log("clicou");
+    const mensagem = {
+        from: nomeUsuario,
+        to: "Todos", // padrão (nomes de usuário é bônus)
+        text: msg,
+        type: "message" // ou "private_message" para o bônus
+    }
+    const promise = axios.post("https://mock-api.driven.com.br/api/vm/uol/messages", mensagem);
+    promise.then((res) => {
+        console.log(res);
+
+        // recupera mensagens e atualiza o chat
+        //atualizaChat();
+
+    });
+    promise.catch((err) => {
+        // em caso de erro, recarrega pagina, pedindo dados novamente
+        console.log("Erro. Enviar mensagem falhou!");
+        window.location.reload();
+    });
+}
+
+function atualizaChat() {
+    const promise = axios.get("https://mock-api.driven.com.br/api/vm/uol/messages");
+    promise.then((res) => {
+        imprimeMensagens(res.data);
+    });
+    promise.catch((err) => {
+        console.log("Erro. Recuperar mensagens falhou!");
+        window.location.reload();
+    });
+}
+
+function atualizaChatPeriodico() {
+    let promise;
+    setInterval(() => {
+        promise = axios.get("https://mock-api.driven.com.br/api/vm/uol/messages");
+        promise.then((res) => {
+            imprimeMensagens(res.data);
+        });
+        promise.catch((err) => {
+            console.log("Erro. Recuperar mensagens falhou!");
+            window.location.reload();
+        });
+    }, 3000);
+}
+
+function imprimeMensagens(mensagens) {
+    const chat = document.querySelector(".chat");
+    let texto;
+    if(mensagens.length > 1){ // se há pelo menos uma mensagem
+        mensagens.forEach(m => {
+            texto += `<div class="mensagem">
+            <p class="mensagem-hora">(${m.time})</p>
+            <p class="mensagem-user">${m.from}</p>
+            <p class="mensagem-conteudo">${m.text}</p>
+            </div>`;
+        });
+        chat.innerHTML = texto;
+    }
+
+}
